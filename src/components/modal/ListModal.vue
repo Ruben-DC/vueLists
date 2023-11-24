853b1e430232e41be835af5e77b3b59a3ac2673d
Ruben DC <decarvalhoruben@proton.me>
2023-11-24 23:29:11 +0100

    Refactor modal component and add form element

--- src/components/modal/ListModal.vue
+++ src/components/modal/ListModal.vue
@@ -1,44 +1,59 @@
 <script setup>
 import Modal from './ModalComponent.vue';
+import { ref } from 'vue';
+
+const handleCancel = () => {
+	modalInstance.value.closeModal();
+};
+
+const handleSubmit = () => {
+	modalInstance.value.closeModal();
+};
+
+const modalInstance = ref(null);
 </script>
 
 <template>
-    <Modal>
-        <template #open-button> Ajouter une liste </template>
+	<Modal ref="modalInstance">
+		<template #open-button> Ajouter une liste </template>
 
-        <template #title> Ajouter une liste </template>
+		<template #title> Ajouter une liste </template>
 
-        <template #content>
-            <label for="list-name">Nom de la liste</label>
-            <input class="modal__input" id="list-name" type="text" placeholder="Nom de la liste" />
-        </template>
+		<template #content>
+			<form></form>
+		</template>
 
-        <template #actions>
-            <button class="modal__cancel-button">Annuler</button>
-            <button class="modal__submit-button" type="submit">Ajouter</button>
-        </template>
-    </Modal>
+		<template #actions>
+			<button class="modal__cancel-button" @click="handleCancel">Annuler</button>
+			<button class="modal__submit-button" @click="handleSubmit">Ajouter</button>
+		</template>
+	</Modal>
 </template>
 
 <style lang="scss" scoped>
 .modal {
-    &__input {
-        line-height: 1.3rem;
-    }
+	&__input {
+		padding: 5px 10px;
 
-    &__submit-button,
-    &__cancel-button {
-        padding: 5px 10px;
+		border: 1px solid $border-color;
+		border-radius: 5px;
 
-        border: 1px solid $border-color;
-        border-radius: 5px;
+		transition: all 0.2s ease-out;
+	}
 
-        transition: all 0.2s ease-out;
-    }
+	&__submit-button,
+	&__cancel-button {
+		padding: 7px 15px;
 
-    &__submit-button {
-        background: $text-color;
-        color: $background-color;
-    }
+		border: 1px solid $border-color;
+		border-radius: 5px;
+
+		transition: all 0.2s ease-out;
+	}
+
+	&__submit-button {
+		background: $text-color;
+		color: $background-color;
+	}
 }
 </style>
