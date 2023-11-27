1ef6691c8c8f1ff36d2d1539d411fe3c2c136849
Ruben DC <rubendcdev@proton.me>
2023-11-27 15:21:23 +0100

    Add list and item functionality to listStore

--- src/stores/listStore.js
+++ src/stores/listStore.js
@@ -1,34 +1,51 @@
 import { defineStore } from 'pinia';
 
+/**
+ * @property {Object} list
+ * @property {string} name
+ * @property {string} description
+ * @property {Object[]} items
+ * @property {Date} id
+ *
+ * @property {Object{}} items.item
+ * @property {string} items.item.name
+ * @property {string} items.item.date
+ */
+
 export const useListsStore = defineStore('lists', {
 	state: () => ({
 		lists: []
 	}),
+
 	getters: {
-		getLists: (state) => state.lists
+		getListById: (state) => (id) => {
+			return state.lists.find((list) => list.id === id);
+		},
+
+		getItemById: (state) => (listId, itemId) => {
+			const list = state.getListById(listId);
+			return list.items.find((item) => item.id === itemId);
+		},
+
+		getItemsByListId: (state) => (listId) => {
+			const list = state.getListById(listId);
+			return list.items;
+		},
+
+		getNumberOfItemsByListId: (state) => (listId) => {
+			const list = state.getListById(listId);
+			return list.items.length;
+		}
 	},
+
 	actions: {
 		addList(list) {
 			this.lists.push(list);
+		},
+
+		addItemToList(listId, item) {
+			const list = this.getListById(listId);
+			list.items.push(item);
 		}
-		// removeList(list) {
-		//     this.lists.splice(this.lists.indexOf(list), 1);
-		// },
-		// updateList(list, newList) {
-		//     this.lists[this.lists.indexOf(list)] = newList;
-		// },
-		// clearLists() {
-		//     this.lists = [];
-		// },
-		// loadLists() {
-		//     this.lists = JSON.parse(localStorage.getItem('lists')) || [];
-		// },
-		// saveLists() {
-		//     localStorage.setItem('lists', JSON.stringify(this.lists));
-		// },
-		// clearAndSaveLists() {
-		//     this.clearLists();
-		//     this.saveLists();
-		// },
 	}
 });
