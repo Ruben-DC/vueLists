d484fceb616faccf65e06bde4a7a2763ea81fa37
Ruben DC <decarvalhoruben@proton.me>
2023-12-17 20:38:59 +0100

    Add @vercel/speed-insights dependency and import SpeedInsights component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
 <script setup>
-import { RouterView } from 'vue-router'
-import Header from '@/layouts/HeaderLayout.vue'
+import { RouterView } from 'vue-router';
+import Header from '@/layouts/HeaderLayout.vue';
+import { SpeedInsights } from '@vercel/speed-insights/vue';
 </script>
 
 <template>
-    <Header />
+	<Header />
 
-    <main>
-        <RouterView />
-    </main>
+	<main>
+		<RouterView />
+	</main>
+
+	<SpeedInsights />
 </template>
 
 <style lang="scss">
 main {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
 
-    width: 100%;
+	width: 100%;
 }
 </style>
