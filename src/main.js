fb2607b7cbcf008014cce0af2b3dc493708138a1
Ruben DC <decarvalhoruben@proton.me>
2023-12-18 13:39:09 +0100

    Add Supabase client configuration

--- src/main.js
+++ src/main.js
@@ -6,12 +6,6 @@ import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import router from './router';
 import App from './App.vue';
 
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://mjlepkeidxeucjvbbnha.supabase.co';
-const supabaseKey = process.env.SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
-
 const app = createApp(App);
 const pinia = createPinia();
 
