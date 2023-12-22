e7ef48db19c6e5d43d1b5d9ab170b7f8a6655410
Ruben DC <decarvalhoruben@proton.me>
2023-12-22 16:45:14 +0100

    Add profile view and implement authentication check

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
+import { useAuthStore } from '../stores/authStore';
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,16 +9,31 @@ const router = createRouter({
 			path: '/',
 			name: 'home',
 			component: HomeView
+		},
+
+		{
+			path: '/profile',
+			name: 'profile',
+			component: () => import('../views/ProfileView.vue'),
+			meta: {
+				needsAuth: true
+			}
 		}
-		// {
-		//   path: '/login',
-		//   name: 'login',
-		//   // route level code-splitting
-		//   // this generates a separate chunk (About.[hash].js) for this route
-		//   // which is lazy-loaded when the route is visited.
-		//   component: () => import('../views/loginView.vue')
-		// }
 	]
+});
+
+router.beforeEach((to, from, next) => {
+	const authStore = useAuthStore();
+
+	if (to.meta.needsAuth) {
+		if (authStore.isLoggedIn()) {
+			return next();
+		} else {
+			return next('/');
+		}
+	}
+
+	return next();
 });
 
 export default router;
---
+++ src/views/ProfileView.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+
+<template>
+	<main class="view">
+		<h1>Mon profil</h1>
+	</main>
+</template>
+
+<style lang="scss" scoped>
+.view {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	gap: 20px;
+
+	max-width: 800px;
+	width: 100%;
+
+	padding: 0 20px 70px 20px;
+}
+</style>
