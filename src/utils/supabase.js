537cb2911d28cd43a75051bdfe9ad158186cd625
Ruben DC <decarvalhoruben@proton.me>
2023-12-22 22:30:34 +0100

    Update Supabase environment variables

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -2,8 +2,8 @@ import { createClient } from '@supabase/supabase-js';
 import { useAuthStore } from '@/stores/authStore';
 import { useUserStore } from '../stores/userStore';
 
-const supabaseUrl = process.env.VITE_SUPABASE_URL;
-const supabaseKey = process.env.VITE_SUPABASE_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
 supabase.auth.onAuthStateChange((event, session) => {
