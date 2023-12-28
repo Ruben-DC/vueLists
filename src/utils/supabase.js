5f580bdad4416b7542e4b07c5bf0af4208f72d81
Ruben DC <rubendcdev@proton.me>
2023-12-28 15:53:55 +0100

    Remove console.log statement in supabase.js

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -10,8 +10,6 @@ supabase.auth.onAuthStateChange((event, session) => {
 	const authStore = useAuthStore();
 	const userStore = useUserStore();
 
-	console.log(event);
-
 	authStore.session = session?.user || null;
 	userStore.getUser();
 });
