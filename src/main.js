23266f5f9f93bbd979d76adbb62272faacd1798e
Ruben DC <rubendcdev@proton.me>
2023-12-28 15:52:08 +0100

    Add @formkit/i18n dependency and formkit styles

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import { createPinia } from 'pinia';
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import router from './router';
 import App from './App.vue';
+import { plugin, defaultConfig } from '@formkit/vue';
+import config from '@/utils/formkit.config.js';
 
 const app = createApp(App);
 const pinia = createPinia();
@@ -12,5 +14,6 @@ const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
 app.use(pinia);
 app.use(router);
+app.use(plugin, defaultConfig(config));
 
 app.mount('#app');
