096998fa1438f4040512b492851cddd76efe1e9e
Ruben DC <decarvalhoruben@proton.me>
2023-12-29 12:19:57 +0100

    Add @iconify/vue dependency and update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,22 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+// import { fileURLToPath, URL } from 'node:url'
+import path from 'path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use "@/assets/styles/colors.scss" as *;`
-      }
-    }
-  }
-})
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			// eslint-disable-next-line no-undef
+			'@': path.resolve(__dirname, './src')
+		}
+	},
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: `@use "@/assets/styles/colors.scss" as *;`
+			}
+		}
+	}
+});
